23e66857565301ebe974dc54be3b487a7d78276c
stefannytoror <stefannyrami@gmail.com>
2019-10-01 21:28:13 -0500

    Filter product by category and brand

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,10 @@ import { ProductDetail } from './../../models/productDetail';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product/product.service';
 
+import { PopoverController } from '@ionic/angular';
+import { PopoverCategoriesComponent } from 'src/app/components/popover-categories/popover-categories.component';
+import { PopoverBrandsComponent } from 'src/app/components/popover-brands/popover-brands.component';
+
 
 @Component({
   selector: 'app-home',
@@ -11,13 +15,16 @@ import { ProductService } from 'src/app/services/product/product.service';
 export class HomePage implements OnInit {
 
   searchText = '';
+  category = '';
+  brand = '';
   products: ProductDetail[];
   productsRuby: ProductDetail[];
   productsGo: ProductDetail[];
   productsFlask: ProductDetail[];
 
   constructor(
-    private productService: ProductService) {}
+    private productService: ProductService,
+    public popoverController: PopoverController) {}
 
   ngOnInit() {
   }
@@ -35,6 +42,80 @@ export class HomePage implements OnInit {
     this.productService.findProductsFlask(this.searchText).subscribe(res => {
       this.products = this.products.concat(res.products);
       this.productsFlask = res.products;
+    });
+  }
+  
+  async presentPopoverCategories(event) {
+    const popover = await this.popoverController.create({
+      component: PopoverCategoriesComponent,
+      event: event,
+      translucent: true
+    });
+
+    popover.onDidDismiss().then((dataReturned) => {
+      if (dataReturned !== null) {
+        this.category = dataReturned.data;
+        this.findProductsByCategory()
+      }
+    });
+    return await popover.present();
+  }
+
+  async presentPopoverBrands(event) {
+    const popover = await this.popoverController.create({
+      component: PopoverBrandsComponent,
+      event: event,
+      translucent: true
+    });
+
+    popover.onDidDismiss().then((dataReturned) => {
+      if (dataReturned !== null) {
+        this.brand = dataReturned.data;
+        this.findProductsByBrand()
+      }
+    });
+    return await popover.present();
+  }
+
+  private findProductsByCategory(){
+    this.products = [];
+    this.productsFlask = this.productsGo = this.productsRuby = []
+
+    this.productService.findProductsByCategoryRuby(this.category).subscribe(res => {
+      this.products.concat(res.products)
+      this.productsRuby = res.products
+      console.log(res.products);
+    });
+    this.productService.findProductsByCategoryFlask(this.category).subscribe(res => {
+      this.products.concat(res.products)
+      this.productsFlask = res.products
+      console.log(res.products);
+    });
+    this.productService.findProductsByCategoryGo(this.category).subscribe(res => {
+      this.products.concat(res.products)
+      this.productsGo = res.products
+      console.log(res.products);
+    });
+  }
+
+  private findProductsByBrand(){
+    this.products = [];
+    this.productsFlask = this.productsGo = this.productsRuby = []
+
+    this.productService.findProductsByBrandRuby(this.brand).subscribe(res => {
+      this.products.concat(res.products)
+      this.productsRuby = res.products
+      console.log(res.products);
+    });
+    this.productService.findProductsByBrandFlask(this.brand).subscribe(res => {
+      this.products.concat(res.products)
+      this.productsFlask = res.brands
+      console.log(res.products);
+    });
+    this.productService.findProductsByBrandGo(this.brand).subscribe(res => {
+      this.products.concat(res.products)
+      this.productsGo = res.products
+      console.log(res.products);
     });
   }
 }
