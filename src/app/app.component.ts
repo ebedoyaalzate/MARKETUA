520a263de1b2b75278ffff24f328f0333544ddbb
stefannytoror <stefannyrami@gmail.com>
2019-10-10 21:48:42 -0500

    fix login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform, MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private menu: MenuController
+    private menu: MenuController,
+    public authService: AuthService
   ) {
     this.initializeApp();
   }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AngularFireAuth } from '@angular/fire/auth';
-import  * as firebase from 'firebase/app';
-import { Observable } from 'rxjs';
-import { GooglePlus } from "@ionic-native/google-plus/ngx";
-import { Platform } from "@ionic/angular";
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -12,51 +10,26 @@ import { Platform } from "@ionic/angular";
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  user: Observable<firebase.User>
+
+  userDetail: any;
 
   constructor(
-    private afAuth: AngularFireAuth,
-    private gPlus: GooglePlus,
-    private platform: Platform) {
-    
-      this.user = this.afAuth.user;
+    private router: Router,
+    public authService: AuthService) {
+      
    }
 
   ngOnInit() {
+    if(this.authService.isLoggedIn()) {
+      console.log("Estoy en login voy para home");
+      
+      this.router.navigate(['/home']);
+    }
   }
+
 
   googleLogin() {
-    if(this.platform.is('cordova')) {
-      this.nativeGoogleLogin();
-    } else {
-      this.webGoogleLogin();
-    }
+    this.authService.googleLogin()
   }
-
-  async nativeGoogleLogin(): Promise<void> {
-    try {
-      const gPlusUser = await this.gPlus.login({
-        'webClientId': '1094856102481-phn3qb7424npnk56prhujpko4b0tp19i.apps.googleusercontent.com',
-        'offline': true,
-        'scopes': 'profile email'
-      });
-      await this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(gPlusUser.idToken))
-    }
-    catch(err) {
-      console.log(err);
-      
-    }
-  }
-
-  async webGoogleLogin(): Promise<void> {
-    try {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      await this.afAuth.auth.signInWithPopup(provider);
   
-    } catch(err) {
-      console.log(err)
-    }
-  
-  }
-
 }
