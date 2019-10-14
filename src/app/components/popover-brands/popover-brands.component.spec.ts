ef3b5c69e44830228015255b7566e4bd489aa197
stefannytoror <stefannyrami@gmail.com>
2019-10-13 21:26:37 -0500

    unit tests

--- src/app/components/popover-brands/popover-brands.component.spec.ts
+++ src/app/components/popover-brands/popover-brands.component.spec.ts
@@ -2,15 +2,22 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PopoverBrandsComponent } from './popover-brands.component';
+import { PopoverController } from '@ionic/angular';
 
 describe('PopoverBrandsComponent', () => {
   let component: PopoverBrandsComponent;
   let fixture: ComponentFixture<PopoverBrandsComponent>;
+  let popoverSpy;
 
   beforeEach(async(() => {
+    popoverSpy = jasmine.createSpyObj('PopoverController', ['']);
+
     TestBed.configureTestingModule({
       declarations: [ PopoverBrandsComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {provide: PopoverController, useValue: popoverSpy},
+      ]
     })
     .compileComponents();
   }));
@@ -21,7 +28,7 @@ describe('PopoverBrandsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create brands component', () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/components/popover-categories/popover-categories.component.spec.ts
+++ src/app/components/popover-categories/popover-categories.component.spec.ts
@@ -2,15 +2,22 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PopoverCategoriesComponent } from './popover-categories.component';
+import { PopoverController } from '@ionic/angular';
 
 describe('PopoverCategoriesComponent', () => {
   let component: PopoverCategoriesComponent;
   let fixture: ComponentFixture<PopoverCategoriesComponent>;
+  let popoverSpy;
 
   beforeEach(async(() => {
+    popoverSpy = jasmine.createSpyObj('PopoverController', ['']);
+
     TestBed.configureTestingModule({
       declarations: [ PopoverCategoriesComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {provide: PopoverController, useValue: popoverSpy},
+      ]
     })
     .compileComponents();
   }));
@@ -21,7 +28,7 @@ describe('PopoverCategoriesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create categories component', () => {
     expect(component).toBeTruthy();
   });
 });
