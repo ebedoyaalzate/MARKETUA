2e11b4e62a25bfe586dc6eabece032249444dc57
ebedoyaalzate <ebedoyaalzate@hotmail.com>
2019-10-18 00:43:32 -0500

    add save car

--- src/app/pages/car/car.page.ts
+++ src/app/pages/car/car.page.ts
@@ -1,6 +1,7 @@
 import { ProductDetail } from '../../models/productDetail';
 import { CarService } from '../../services/car/car.service';
 import { Component, OnInit, Input } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-car',
@@ -9,23 +10,55 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CarPage implements OnInit {
 
-  @Input() product: ProductDetail;
   productos: any;
+  showSave = true;
 
   constructor(
     private carService: CarService,
+    private alertController: AlertController
   ) { }
 
   ngOnInit() {
     this.getCar();
     this.carService.changes.subscribe(product => {
       this.productos = this.productos.filter(prod => prod.id !== product.id && prod.prov !== product.prov);
+      this.showSave = false;
     });
   }
 
   getCar() {
     this.carService.getCar().then(prod => {
       this.productos = prod;
+      if (this.productos.length === 0) {
+        this.showSave = false;
+      }
     });
+  }
+
+  saveCar() {
+    this.presentAlert();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Guardar Carrito',
+      message: '¿Esta seguro de guardar el carro?',
+      buttons: [
+        {
+          text: 'Cancel',
+          cssClass: 'secondary'
+        },
+        {
+          text: 'OK',
+          cssClass: 'primary',
+          handler: () => {
+            this.carService.saveCar(this.productos).subscribe(res => {
+              console.log(res);
+            })
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 }
--- src/app/services/car/car.service.ts
+++ src/app/services/car/car.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Subject, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from 'src/app/services/auth/auth.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +13,11 @@ export class CarService {
   private subject = new Subject<any>();
   public changes = this.subject.asObservable();
 
-  constructor(private storage: Storage) { }
+  constructor(
+    private storage: Storage,
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
 
   addToCar(id: string, prov: string) {
     const prod = {
@@ -44,5 +51,24 @@ export class CarService {
 
   deleteCar() {
     this.storage.clear();
+  }
+
+  saveCar(car) {
+    let formatCar
+    formatCar = {
+      items: [{
+        item_id: car[0].id,
+        quantity: car[0].units,
+        backend: car[0].prov
+      }],
+      username: this.authService.userDetails.email.split('@')[0]
+    };
+    if (formatCar.items[0].backend === 'ruby') {
+      return this.http.post<any>('http://marketua-develop-api.herokuapp.com/save-cart/', formatCar);
+    } else if (formatCar.items[0].backend === 'go') {
+      return this.http.post<any>('https://marketua-go-api.herokuapp.com/save-cart', formatCar);
+    } else if (formatCar.items[0].backend === 'flask') {
+      return this.http.post<any>('https://marketuaflask.herokuapp.com/save-cart', formatCar);
+    }
   }
 }
