760a834075b1e81695c5ef47923f74d6ee72cae4
Taiga10 <fernanda.fujosh1@gmail.com>
2019-10-18 21:20:43 -0500

    commit cypress

--- cypress/integration/marketua.spec.js
+++ cypress/integration/marketua.spec.js
@@ -1,6 +1,73 @@
 describe('Marketua tests', function() {
     it('Marketua runs', function() {
-        cy.visit('http://localhost:8100/')
+        cy.visit('http://localhost:8100/login')
     })
 
+    it('Login', function() {
+        cy.visit('http://localhost:8100/login')
+        cy.get('.center')
+        .click()
+    })
+
+    it('Search', function() {
+        cy.visit('http://localhost:8100/home')
+        cy.get('[aria-labelledby="ion-input-0-lbl"]')
+            .type('celular')
+            .should('have.value', 'celular')
+        cy.get('ion-col.md > .ion-color-primary')
+            .click()
+        cy.contains('Celular Huawei')
+    })
+
+    it('Category', function() {
+        cy.visit('http://localhost:8100/home')
+        cy.get('ion-col.md > :nth-child(3)')
+            .click()
+        cy.get('.popover-viewport > .list-md > :nth-child(1)')
+            .click()
+        cy.contains('Xiaomi Redmi')
+    })
+
+    it('Marca', function() {
+        cy.visit('http://localhost:8100/home')
+        cy.get('ion-col.md > :nth-child(4)')
+            .click()
+        cy.get('.popover-viewport > .list-md > :nth-child(1)')
+            .click()
+        cy.contains('Portatil Acer')
+    })
+
+    it('Detalle', function() {
+        cy.visit('http://localhost:8100/home')
+        cy.get('ion-col.md > .ion-color-primary')
+            .click()
+        cy.get(':nth-child(2) > .card')
+            .click()
+            //me falta que si cargue y validar la url
+    })
+    
+    it('Agregar carrito desde buscar', function() {
+        cy.visit('http://localhost:8100/home')
+        cy.get('ion-col.md > .ion-color-primary')
+            .click()
+        cy.get(':nth-child(1) > .card > .content > .button')
+            .click()
+        cy.get('.alert-button-inner')
+            .click()
+        //falta poder ver que si diga exitoso
+    })
+
+    it('Ver carrito', function() {
+        cy.visit('http://localhost:8100/home')
+        cy.get('ion-col.md > .ion-color-primary')
+            .click()
+        cy.get(':nth-child(1) > .card > .content > .button')
+            .click()
+        //cy.contains('Exitoso')
+        cy.get('.footer-md > .ion-color')
+            .click()
+        cy.url().should('eq', 'http://localhost:8100/car')
+        //falta poder ver que si diga exitoso y tenga algo
+    })
+    
 })
